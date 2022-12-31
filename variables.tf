5216c575504bfb353242bdcd73ab3e44433bd9eb
Sonikeh <111326211+Sonikeh@users.noreply.github.com>
2022-12-31 15:35:18 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+
+variable "instance_type" {
+
+   description = "This is for defining instance type of server"
+   default = "t2-micro"
+
+}
+
+variable "ami" {
+
+   description = "The ami of the instance"
+
+}
+
+
+
+variable "tag" {
+
+
+   description = "name of the instance"
+
+}
